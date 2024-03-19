de9554fcfb4d51f0758c608ca96bcef3dd228044
Anna <wenan@staff.1111.com.tw>
2024-03-19 18:02:03 +0800

    新增 DataSource 分 Local、Remote

---
+++ app/src/main/java/com/anna/homeworkandroidinterview/core/repository/source/local/ImagesLocalDataSource.kt
@@ -0,0 +1,18 @@
+package com.anna.homeworkandroidinterview.core.repository.source.local
+
+import com.anna.homeworkandroidinterview.core.repository.ImageDataSource
+import com.anna.homeworkandroidinterview.data.model.response.SearchImageResponseData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class ImagesLocalDataSource : ImageDataSource {
+    override fun searchImage(
+        onError: (String) -> Unit,
+        keywords: String
+    ): Flow<SearchImageResponseData> {
+        return flow {
+            emit(SearchImageResponseData(0, 0, arrayListOf()))
+        }
+    }
+
+}
